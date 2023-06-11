c28f04b4dc1d35dd16a82cb2d78915b607e0ca86
garnere1 <emilygarner24@gmail.com>
2023-06-11 14:03:39 -0400

    Colors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React from "react";
 import Tabs from "./Tabs/Tabs";
 import headshot from "./Images/headshot.jpeg";
+import { TypeAnimation } from 'react-type-animation';
+
 
 function App() {
   return (
@@ -12,6 +14,22 @@ function App() {
           <h1>
             Emily Garner
           </h1>
+          <p>
+            <TypeAnimation
+              sequence={[
+                'Programmer',
+                1000,
+                'Ideator', 
+                1000,
+                'Innovator', 
+                1000,
+              ]}
+              wrapper="span"
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: '2em', display: 'inline-block' }}
+            />
+          </p>
       </div>
       </header>
       <Tabs />  
