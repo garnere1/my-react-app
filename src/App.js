23ef3e491329678a0f96b4d83819c9e41dc949d2
garnere1 <emilygarner24@gmail.com>
2023-06-11 17:58:51 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import "./App.css";
-import React from "react";
-import Tabs from "./Tabs/Tabs";
-import headshot from "./Assets/headshot.jpeg";
-import { TypeAnimation } from 'react-type-animation';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={headshot} className="headshot" alt="headshot" />
-        <div className="headerText">
-          <h1>
-            Emily Garner
-          </h1>
-          <p>
-            <TypeAnimation
-              sequence={[
-                'Programmer',
-                1000,
-                'Ideator', 
-                1000,
-                'Innovator', 
-                1000,
-              ]}
-              wrapper="span"
-              cursor={true}
-              repeat={Infinity}
-              style={{ fontSize: '2em', display: 'inline-block' }}
-            />
-          </p>
-      </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <Tabs />  
     </div>
   );
 }
+
 export default App;
--- src/Tabs/TabComponents/Projects.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Projects = () => {
-  return (
-    <div className="Projects">
-      <p>Projects Section</p>
-    </div>
-  );
-};
-export default Projects;
